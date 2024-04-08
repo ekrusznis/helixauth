3e6d56b85b0bae3a896ed60cf6d979d2bdcde5f0
ekrusznis <ekrusznis@gmail.com>
2024-04-08 11:48:26 -0400

    updated flyway - ek

--- src/main/kotlin/com/ha/helixauth/api/user/repository/UserRepository.kt
+++ src/main/kotlin/com/ha/helixauth/api/user/repository/UserRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmailAndPassword(email: String, password: String): User
-    fun findByToken(token: String): User
+    fun findBySessionToken(token: String): User
 }
